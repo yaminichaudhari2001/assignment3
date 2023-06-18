cecf16691243366eb0250d509b5016a9e4bbedb9
yami <yaminichaudhari2001@gmail.com>
2023-06-18 23:41:52 +0530

    new-assignment3

---
+++ asg1/src/Main.java
@@ -0,0 +1,10 @@
+// Java Program to print the elements of an array
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = { 0, 3, 20, 25, 12,100 };
+        System.out.print("Elements of given array are: ");
+         for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+    }
+}
+}
