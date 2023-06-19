6c44c2c2338a1e3d978b675b982378bd039e490b
yaminichaudhari2001 <130443234+yaminichaudhari2001@users.noreply.github.com>
2023-06-19 15:06:33 +0530

    Update1 Main.java

--- assign7/src/Main.java
+++ assign7/src/Main.java
@@ -1,7 +1,7 @@
 //7) Java Program to print the elements of an array present on odd position
 public class Main {
     public static void main(String[] args) {
-        int[] arr = new int[] { 1, 2, 3, 4, 5, 6 };
+        int[] arr = new int[ { 1, 2, 3, 4, 5, 6 };
 
         for (int i = 0; i < arr.length; i = i + 2) {
 
