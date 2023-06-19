5f632d8bc779e6fffccaf9ecffddc807d6242576
yaminichaudhari2001 <130443234+yaminichaudhari2001@users.noreply.github.com>
2023-06-19 14:59:41 +0530

    Update2 Main.java

--- assign10/src/Main.java
+++ assign10/src/Main.java
@@ -1,7 +1,7 @@
 //10) Find 2nd Largest Number in an Array
 public class Main {
     public static void main(String[] args) {
-        int [] arr ={5, 80, 8, 7, 11};
+        int [] arr = {5, 80, 8, 7, 11};
         int temp = 0;
         System.out.println("Elements of your array: ");
         for (int i = 0; i < arr.length; i++) {
